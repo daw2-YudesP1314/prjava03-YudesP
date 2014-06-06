0f77a0fc65b64170ef4f2159d8b2bb502d21b619
ubu64 <ubu64@ubu64DVL-Yudes.F>
2014-06-06 22:08:45 +0200

    Versió 0.6-C del projecte prjava03.YudesP. Branca00 modificada

--- src/prjava03/yudesp/Prjava03YudesP.java
+++ src/prjava03/yudesp/Prjava03YudesP.java
@@ -4,6 +4,7 @@ package prjava03.yudesp;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.text.*;
 
 public class Prjava03YudesP extends Frame
 implements ActionListener, WindowListener {
